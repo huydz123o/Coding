26bfa593c46db21e0ce568732bd228be026cae28
unknown <huyquang7898@gmail.com>
2023-05-02 12:07:02 +0700

    update files

---
+++ Vinhdinh Coder/main.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+
+ll gt(ll a)
+{
+    if (a == 1) return 1;
+    return a * gt(a - 1);
+}
+ll b[10001];
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a = gt(n);
+    ll cnt = 0;
+    for (ll i = 1; i * i <= a; i++)
+    {
+        if (a % i == 0)
+        {
+            cnt++;
+            if (i != a / i)
+            {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("main.inp", "r", stdin);
+    freopen("main.out", "w", stdout);
+#endif
+
+    ll t = 1;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
